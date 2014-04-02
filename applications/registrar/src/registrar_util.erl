9a25fd40f37ef9acb1b08883f5daea8159c02406
lazedo <luis.azedo@factorlusitano.com>
2014-04-02 17:15:05 +0100

    missing file

---
+++ applications/registrar/src/registrar_util.erl
@@ -0,0 +1,36 @@
+%% @author root
+%% @doc @todo Add description to registrar_util.
+
+
+-module(registrar_util).
+
+-include("reg.hrl").
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-export([bin_to_hexstr/1, hexstr_to_bin/1]).
+
+bin_to_hexstr(Bin) ->
+  lists:flatten([io_lib:format("~2.16.0B", [X]) ||
+    X <- binary_to_list(Bin)]).
+
+-spec hexstr_to_bin(binary() | list()) -> binary().
+hexstr_to_bin(S) when is_binary(S) ->
+    hexstr_to_bin(binary_to_list(S));
+hexstr_to_bin(S) ->
+  hexstr_to_bin(S, []).
+hexstr_to_bin([], Acc) ->
+  list_to_binary(lists:reverse(Acc));
+hexstr_to_bin([X,Y|T], Acc) ->
+  {ok, [V], []} = io_lib:fread("~16u", [X,Y]),
+  hexstr_to_bin(T, [V | Acc]).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
