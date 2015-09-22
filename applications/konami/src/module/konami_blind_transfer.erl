9617705e01d08ad283246736b45f9de3da1b42c0
SIPLABS, LLC <info@siplabs.ru>
2015-09-22 18:33:54 +0700

    blind transfer for konami
    Change-Id: Iad80e1280f8a68887be34c2a2c773cb83a169677

---
+++ applications/konami/src/module/konami_blind_transfer.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2014-2015, 2600Hz
+%%% @doc
+%%% Transfers caller to the extension extracted in the regex
+%%% Data = {
+%%%   "target":"1000" // extension/DID to transfer to, optional
+%%% }
+%%% @end
+%%% @contributors
+%%%   SIPLABS LLC (Maksim Krzhemenevskiy)
+%%%-------------------------------------------------------------------
+-module(konami_blind_transfer).
+
+%% API
+-export([handle/2]).
+
+-spec handle(wh_json:object(), whapps_call:call()) -> no_return().
+handle(Data, Call) ->
+    Transferor = wh_json:get_value(<<"dtmf_leg">>, Data),
+    Transferee =
+        case whapps_call:call_id(Call) of
+            Transferor -> whapps_call:other_leg_call_id(Call);
+            CallId -> CallId
+        end,
+    Extension = wh_json:get_first_defined([<<"captures">>, <<"target">>], Data),
+    whapps_call_command:transfer(Extension, Transferee, Call),
+    {'continue', Call}.
