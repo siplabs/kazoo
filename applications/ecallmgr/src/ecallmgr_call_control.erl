siplabs/kazoo

commit d97a7dc68ac8b3b25c97d2598f0dcc485c7d9b5a

request a control queue with for call-id. Cherry-picked from voxter, contains modifications.
Change-Id: Ic25cac7bc4706898d667626a85185a89159f6e8e

applications/ecallmgr/src/ecallmgr_call_control.erl
@@ -113,7 +113,7 @@
                        ,[{<<"call_event">>, <<"*">>}]
                       }
                     ]).
--define(QUEUE_NAME, <<>>).
+-define(QUEUE_NAME(CallId), <<"call_ctl_", CallId/binary>>).
 -define(QUEUE_OPTIONS, []).
 -define(CONSUME_OPTIONS, []).
 
@@ -144,7 +144,7 @@ start_link(Node, CallId, FetchId, ControllerQ, CCVs) ->
                ],
     gen_listener:start_link(?MODULE, [{'responders', ?RESPONDERS}
                                       ,{'bindings', Bindings}
-                                      ,{'queue_name', ?QUEUE_NAME}
+                                      ,{'queue_name', ?QUEUE_NAME(CallId)}
                                       ,{'queue_options', ?QUEUE_OPTIONS}
                                       ,{'consume_options', ?CONSUME_OPTIONS}
                                      ]
@@ -246,6 +246,7 @@ init([Node, CallId, FetchId, ControllerQ, CCVs]) ->
     put('callid', CallId),
     lager:debug("starting call control listener"),
     gen_listener:cast(self(), 'init'),
+    ecallmgr_fs_channels:update(CallId, #channel.control_q, ?QUEUE_NAME(CallId)),
     {'ok', #state{node=Node
                   ,call_id=CallId
                   ,command_q=queue:new()
@@ -761,7 +762,8 @@ add_leg(Props, LegId, #state{other_legs=Legs
             _ = spawn(fun() ->
                               _ = put('callid', CallId),
                               wh_amqp_channel:consumer_pid(ConsumerPid),
-                              publish_leg_addition(props:set_value(<<"Other-Leg-Unique-ID">>, CallId, Props))
+                              publish_leg_addition(props:set_value(<<"Other-Leg-Unique-ID">>, CallId, Props)),
+                              ecallmgr_fs_channels:update(LegId, #channel.control_q, ?QUEUE_NAME(CallId))
                       end),
             State#state{other_legs=[LegId|Legs]}
     end.

applications/ecallmgr/src/ecallmgr.hrl
@@ -80,6 +80,7 @@
                   ,handling_locally = 'false' :: boolean() | '_' %% is this ecallmgr handling the call control?
                   ,to_tag :: api_binary() | '_'
                   ,from_tag :: api_binary() | '_'
+                  ,control_q :: api_binary() | '_'
                  }).
 
 -type channel() :: #channel{}.

applications/ecallmgr/src/ecallmgr_fs_channel.erl
@@ -210,6 +210,7 @@ to_props(Channel) ->
        ,{<<"to_tag">>, Channel#channel.to_tag}
        ,{<<"from_tag">>, Channel#channel.from_tag}
        ,{<<"elapsed_s">>, wh_util:elapsed_s(Channel#channel.timestamp)}
+       ,{<<"control_q">>, Channel#channel.control_q}
       ]).
 
 -spec to_api_json(channel()) -> wh_json:object().
@@ -247,6 +248,7 @@ to_api_props(Channel) ->
        ,{<<"From-Tag">>, Channel#channel.from_tag}
        ,{<<"Switch-URL">>, ecallmgr_fs_nodes:sip_url(Channel#channel.node)}
        ,{<<"Elapsed-Seconds">>, wh_util:elapsed_s(Channel#channel.timestamp)}
+       ,{<<"Control-Queue">>, Channel#channel.control_q}
       ]).
 
 %%%===================================================================
@@ -526,6 +528,7 @@ props_to_record(Props, Node) ->
              ,handling_locally=handling_locally(Props)
              ,to_tag=props:get_value(<<"variable_sip_to_tag">>, Props)
              ,from_tag=props:get_value(<<"variable_sip_from_tag">>, Props)
+             ,control_q=props:get_value(<<"control_q">>, Props)
             }.
 
 -spec handling_locally(wh_proplist()) -> boolean().
@@ -572,6 +575,7 @@ props_to_update(Props) ->
                             ,{#channel.dialplan, props:get_value(<<"Caller-Dialplan">>, Props)}
                             ,{#channel.to_tag, props:get_value(<<"variable_sip_to_tag">>, Props)}
                             ,{#channel.from_tag, props:get_value(<<"variable_sip_from_tag">>, Props)}
+                            ,{#channel.control_q, props:get_value(<<"variable_control_q">>, Props)}
                            ]).
 
 -spec get_other_leg(ne_binary(), wh_proplist()) -> api_binary().

commit 95446e751b7788089301f85c63495353f4fbd0d1

do not broke blind_transfer with d97a7dc
Change-Id: I6cdb52ae27872b8decf138c8841b3522b88f3d56

applications/ecallmgr/src/ecallmgr_call_control.erl
@@ -113,7 +113,7 @@
                        ,[{<<"call_event">>, <<"*">>}]
                       }
                     ]).
--define(QUEUE_NAME(CallId), <<"call_ctl_", CallId/binary>>).
+-define(QUEUE_NAME, <<>>).
 -define(QUEUE_OPTIONS, []).
 -define(CONSUME_OPTIONS, []).
 
@@ -144,7 +144,7 @@ start_link(Node, CallId, FetchId, ControllerQ, CCVs) ->
                ],
     gen_listener:start_link(?MODULE, [{'responders', ?RESPONDERS}
                                       ,{'bindings', Bindings}
-                                      ,{'queue_name', ?QUEUE_NAME(CallId)}
+                                      ,{'queue_name', ?QUEUE_NAME}
                                       ,{'queue_options', ?QUEUE_OPTIONS}
                                       ,{'consume_options', ?CONSUME_OPTIONS}
                                      ]
@@ -246,7 +246,6 @@ init([Node, CallId, FetchId, ControllerQ, CCVs]) ->
     put('callid', CallId),
     lager:debug("starting call control listener"),
     gen_listener:cast(self(), 'init'),
-    ecallmgr_fs_channels:update(CallId, #channel.control_q, ?QUEUE_NAME(CallId)),
     {'ok', #state{node=Node
                   ,call_id=CallId
                   ,command_q=queue:new()
@@ -319,7 +318,8 @@ handle_cast({'gen_listener', {'created_queue', _}}
            ) ->
     lager:debug("call control got created_queue but controller is undefined"),
     {'noreply', State};
-handle_cast({'gen_listener', {'created_queue', Q}}, State) ->
+handle_cast({'gen_listener', {'created_queue', Q}}, #state{call_id = CallId} = State) ->
+    ecallmgr_fs_channels:update(CallId, #channel.control_q, Q),
     {'noreply', State#state{control_q=Q}};
 handle_cast({'gen_listener', {'is_consuming', _IsConsuming}}
             ,#state{controller_q='undefined'}=State
@@ -753,6 +753,7 @@ handle_channel_create(Props, #state{call_id=CallId}=State) ->
 -spec add_leg(wh_proplist(), ne_binary(), state()) -> state().
 add_leg(Props, LegId, #state{other_legs=Legs
                              ,call_id=CallId
+                             ,control_q = ControlQ
                             }=State) ->
     case lists:member(LegId, Legs) of
         'true' -> State;
@@ -763,7 +764,7 @@ add_leg(Props, LegId, #state{other_legs=Legs
                               _ = put('callid', CallId),
                               wh_amqp_channel:consumer_pid(ConsumerPid),
                               publish_leg_addition(props:set_value(<<"Other-Leg-Unique-ID">>, CallId, Props)),
-                              ecallmgr_fs_channels:update(LegId, #channel.control_q, ?QUEUE_NAME(CallId))
+                              ecallmgr_fs_channels:update(LegId, #channel.control_q, ControlQ)
                       end),
             State#state{other_legs=[LegId|Legs]}
     end.